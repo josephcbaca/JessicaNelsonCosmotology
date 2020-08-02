3cb01da3c3c0a0930226d992a562b258433cc287
Joseph Baca <josephcbaca@gmail.com>
2020-08-02 14:21:16 -0700

    Added Copy Email function on Contact page.

---
+++ index.js
@@ -0,0 +1,30 @@
+// Gallyer Page
+
+
+
+
+
+
+
+// Contact Page
+$("#copy-text").on("click", function () {
+    copyToClipBoard("josephcbaca@gmail.com");
+    popUpClipBoard()
+});
+
+const copyToClipBoard = (str) => {
+    const el = document.createElement("textarea");
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+};
+
+function popUpClipBoard() {
+    $("#popUp").show();
+    setTimeout(function () {
+        $("#popUp").hide();
+    }, 300);
+
+  }
